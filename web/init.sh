be972be003761768aa457612d4ba4a2d0aa5ca5f
Maxon <someemailmoi@yo.com>
2023-01-25 08:48:07 -0500

    moved nginx.conf to etc

--- web/init.sh
+++ web/init.sh
@@ -1,3 +1,12 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo pip3 install virtualenv
+sudo pip3 install pathlib2
+virtual env -p python3 venv
+source venv/bin/activate
+pip3 install django
+
 sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf
+
 sudo /etc/init.d/nginx restart
+
+gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app & gunicorn -w 2 -c /home/box/web/etc/qa.py ask.wsgi:application & curl -vv 'http://127.0.0.1:8000/login/'
